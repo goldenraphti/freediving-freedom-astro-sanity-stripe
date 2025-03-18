fdc160b7b8c4fa8d243820acbdc70ac392c63c9b
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-03-18 17:22:53 +0100

    fix type dataset sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [
     sanity({
       projectId: "axbwgidy",
-      dataset: "freediving-freedom-new_dataset",
+      dataset: "feediving-freedom-new_dataset",
       useCdn: false, // See note on using the CDN
       apiVersion: "2025-03-18", // insert the current date to access the latest version of the API,
       studioBasePath: "/studio", // If you want to access the Studio on a route
