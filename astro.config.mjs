03266bd4164a91d9b91c719bd39bffe66eec86de
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-03-18 16:50:48 +0100

    add sanity basic studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity(), react()]
+  integrations: [
+    sanity({
+      projectId: "axbwgidy",
+      dataset: "freediving-freedom-new_dataset",
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2025-03-18", // insert the current date to access the latest version of the API,
+      studioBasePath: "/studio", // If you want to access the Studio on a route
+    }),
+    react(),
+  ],
 });
